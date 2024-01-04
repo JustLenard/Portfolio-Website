128846723a71831fb3c237566fa34c49ad2b80ad
JustLenard <justlenard.justme@gmail.com>
2024-01-04 20:53:08 +0200

    Fixed CV download

--- components/intro.tsx
+++ components/intro.tsx
@@ -98,8 +98,9 @@ export default function Intro() {
 
 				<a
 					className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-					href="/public/Cociug_Vitalie_Full_Stack_Developer.pdf"
-					download
+					href="/Cociug_Vitalie_Full_Stack_Developer.pdf"
+					target="_blank"
+					rel="noopener noreferrer"
 				>
 					Download CV
 					<HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
